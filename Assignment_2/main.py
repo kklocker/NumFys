160a769f3b0b30f88145bf33bc0885af0bbad3b4
Karl Kristian Lockert <karladlock@gmail.com>
2020-03-15 19:19:47 +0100

    wwwwwwww

--- Assignment_2/main.py
+++ Assignment_2/main.py
@@ -1,28 +1,37 @@
-import autograd.numpy as np
+import numpy as np
+
+# import autograd.numpy as np
 from autograd import elementwise_grad as egrad
 from dask import delayed, compute
-from numba import jit
+from numba import jit, njit
 import matplotlib.pyplot as plt
+from tqdm import trange
+from time import time
+
 
 from utils import U, force, rand_gauss, get_time_step, normalized_constants
 
 kb = 1.3806e-23
 
 
-@jit(nopython=False, forceobj=True)
-def euler_scheme(x, n, dt, D):
-    """Returns the *next* point in time-domain
+@njit  # (nopython=False, forceobj=True)
+def euler_scheme(x, n, dt, D, F):
+    # du = force(x, n * dt, 0.2, 1, flashing)
+    ksi = rand_gauss(x.shape[0])
+    # print(F.shape)
+    return x - F * dt + np.sqrt(2 * D * dt) * ksi
+
+
+def boltzmann(U, du, kbT):
+    """
+    Returns a boltzmann distribution from U
     
     Arguments:
-        x {float} -- Position at time t
-        t {float} -- time
-        dt {float} -- time step
+        U {[type]} -- [description]
+        du {[type]} -- [description]
+        KbT {[type]} -- [description]
     """
-
-    du = force(x, n * dt, 0.2, 1)
-    ksi = rand_gauss(x.shape[0])
-
-    return x - du * dt + np.sqrt(2 * D * dt) * ksi
+    return np.exp(-(U * du) / kbT) / (kbT * (1 - np.exp(-(du / kbT))))
 
 
 # def simulate_particle():
@@ -42,6 +51,14 @@ def euler_scheme(x, n, dt, D):
 #     """
 
 
+def plot_position_distribution(t_list, avg_pos_list):
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(t_list, avg_pos_list)
+    # plt.savefig("")
+    plt.show()
+
+
 if __name__ == "__main__":
 
     data = {
@@ -49,11 +66,29 @@ if __name__ == "__main__":
         "L": 20e-6,
         "alpha": 0.2,
         "eta": 1e-3,
-        "kbt": 26e-3,
+        "kbt": 1.60 * 26e-22,
         "delta_u": 80 * 1.60e-19,
     }
 
-    gamma, omega, D = normalized_constants()
-    dt = get_time_step(data["alpha"], D, tol=0.1)
-    N = 10000  # number of steps
+    alpha = data["alpha"]
+    flashing = False
+    gamma, omega, D = normalized_constants(**data)
+    dt = get_time_step(alpha, D, tol=0.1)
+    print(f"Time step: {dt}")
 
+    N_steps = 100000  # number of steps
+    N_particles = 1000
+
+    start = time()
+
+    particle_positions = np.zeros(N_particles)
+    avg_particle_positions = np.zeros(N_steps)
+    for n in range(N_steps):
+        f = force(particle_positions, n * dt, alpha, 1.0, flashing)
+        particle_positions = euler_scheme(particle_positions, n, dt, D, f)
+        avg_particle_positions[n] = np.average(particle_positions)
+
+    stop = time()
+    print(f"Total time: {stop-start}")
+    t_list = np.linspace(0, N_steps * dt, N_steps)
+    plot_position_distribution(t_list, avg_particle_positions)
--- Assignment_2/utils.py
+++ Assignment_2/utils.py
@@ -1,22 +1,23 @@
-# import numpy as np
+import numpy as npp
 import autograd.numpy as np
 from autograd import elementwise_grad as egrad
 from dask import delayed, compute
-from numba import jit
+from numba import jit, njit, vectorize, float64, boolean
 import matplotlib.pyplot as plt
 
 
-@jit(nopython=False)
+# @jit(nopython=False, forceobj=True)
+# @njit
 def Ur(x, alpha):
     """Help function for the spatial part of the normalized potential
     """
     k1 = 1 / alpha
     k2 = 1 / (1 - alpha)
     a = x % 1.0
-    return np.where(a < alpha, k1 * a, k2 * (1 - a))
+    return npp.where(a < alpha, k1 * a, k2 * (1 - a))
 
 
-@jit(nopython=False)
+# @jit(nopython=True)
 def f(t, tau, flashing=True):
     """Help funtion for time-part of potential
     
@@ -28,11 +29,9 @@ def f(t, tau, flashing=True):
         array -- [description]
     """
 
-    l = np.ones_like(t)
-    if not flashing:
-        return l
+    l = npp.ones_like(t)
     a = t % tau
-    return np.where(a < 3 * tau / 4, 0 * l, l)
+    return npp.where(a < 3 * tau / 4, 0 * l, l)
 
 
 @jit(nopython=False, forceobj=True)
@@ -42,14 +41,30 @@ def U(x, t, alpha, tau, flashing=True):
     Returns:
         [type] -- [description]
     """
-    return Ur(x, alpha) * f(t, tau, flashing)
+    if not flashing:
+        return Ur(x, alpha)
+    return Ur(x, alpha) * f(t, tau)
 
 
-# @jit(nopython=False)
-def force(x, t, alpha, tau):
-    return -egrad(U, 0)(x, t, alpha, tau)
+# @vectorize([float64(float64, float64)])
+# @jit(nopython=True)
+def force_x(x, alpha):
+    k1 = 1 / alpha
+    k2 = 1 / (1 - alpha)
+    a = x % 1.0
+    return npp.where(a < alpha, k1, -k2)
 
 
+# @vectorize([float64(float64, float64, float64, float64, boolean)])
+@jit(nopython=False, forceobj=True)
+def force(x, t, alpha, tau, flashing=True):
+    # return -egrad(U, 0)(x, t, alpha, tau, flashing)
+    if not flashing:
+        return force_x(x, alpha)
+    return force_x(x, alpha) * f(t, tau)
+
+
+@jit(nopython=True)
 def rand_gauss(N):
     """Returns a simple gaussian centered around 0 with standard deviation 1
     
@@ -59,23 +74,22 @@ def rand_gauss(N):
     Returns:
         [type] -- [description]
     """
-    return np.random.normal(0, 1, N)
+    return npp.random.normal(0, 1, N)
 
 
 def get_time_step(alpha, D, tol=0.1):
     """
     Returns a time step that satisfies the criterion for the normalized problem.
     """
-    M = np.max(np.abs(force(np.linspace(0, 1, 100), 0.0, alpha, 1.0)))
+    M = npp.max(npp.abs(force(npp.linspace(0, 1, 100), 0.0, alpha, 1.0)))
 
     dt = 0.1
-    while not g(dt, M, D) < tol * alpha:
+    while not time_step_condition(dt, M, D) < tol * alpha:
         dt = dt / 2
     return dt
 
 
-@jit(nopython=False)
-def g(dt, M, D):
+def time_step_condition(dt, M, D):
     """
     Help function for determining time step.
     """
@@ -91,7 +105,7 @@ def normalized_constants(**kwargs):
     eta = kwargs.get("eta")
     kbt = kwargs.get("kbt")
     delta_u = kwargs.get("delta_u")
-
+    print(kwargs)
     gamma = 6 * np.pi * eta * r
     omega = delta_u / (gamma * L ** 2)
     D = kbt / delta_u
