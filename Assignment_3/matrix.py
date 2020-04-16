a93dc98a021ecafcc085bd1dba7a8e441b6ca14b
Karl Kristian Lockert <karladlock@gmail.com>
2020-04-16 21:40:58 +0200

    Barely started

---
+++ Assignment_3/matrix.py
@@ -0,0 +1,36 @@
+from scipy.sparse import diags
+from scipy.sparse.linalg import eigsh
+import numpy as np
+
+
+def matrix_box_potential(N, order=4):
+    """Sets up the one-dimensional eigenvalue solver
+    
+    """
+    x = np.ones(N) * N ** order
+
+    if order == 2:
+        d0 = 2 * x
+        d1 = -x[:-1]
+        A = diags([d1, d0, d1], [-1, 0, 1], dtype=complex)
+
+    elif order == 4:
+
+        d0 = 5 * x / 2
+        d1 = -4 * x[:-1] / 3
+        d2 = x[:-2] / 12
+
+        A = diags([d2, d1, d0, d1, d2], [-2, -1, 0, 1, 2], dtype=complex)
+
+    elif order == 6:
+        d0 = 49 * x / 18
+        d1 = -3 * x[:-1] / 2
+        d2 = 3 * x[:-2] / 20
+        d3 = -x[:-3] / 90
+
+        A = diags([d3, d2, d1, d0, d1, d2, d3], [-3, -2, -1, 0, 1, 2, 3], dtype=complex)
+
+    else:
+        raise NotImplementedError
+
+    return A
